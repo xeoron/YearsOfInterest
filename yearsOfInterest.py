df6255ceb9db3fbf60df4f098354ee5e117d4a23
Jason Campisi <aitsinformation@gmail.com>
2023-06-13 18:21:01 -0400

    Initial commit

---
+++ yearsOfInterest.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# Name: yearsOfInterest.py
+# License: Released under GPL v3 or higher. Details here http://www.gnu.org/licenses/gpl.html
+# Author: Jason Campisi
+# Version 0.0.1
+# Date: 6/13/23
+# About: This program solves if you have x amount of money that grows in interest each year at y rate, how much do you have after z years?
+# Usage: yoi amount-of-money interest-rate [years]
+#  Where interest rate is in decimal form
+#  if you do not define years, it will assume 30
+
+from ast import Return
+import sys
+
+def addFormatting(number):  # add comas to large nunbers and round to 2 digital before the decimal point
+    return ("{:,.2F}".format(number))
+def percentage(number):     # convert digit percentage to number thus 0.05 percent is converted to 5%
+    return "{0:.0f}%".format(number * 100)   
+
+try:
+    if len(sys.argv) < 2: #aka sys.argv[1] is empty  #no value of money?
+        print ("Error: money value not provided")
+        exit(1)
+    elif len(sys.argv) < 3: #aka sys.argv[2] is empty #no interest rate?
+        print ("Error: No interest rate provided")
+        exit(1)
+    else:
+        money = float(sys.argv[1])
+        interest = float(sys.argv[2])
+        
+    if len(sys.argv) < 4:   #no years of interest?
+        years = 30
+    else:
+        years = int(sys.argv[3])
+    
+    for y in range (years):
+        money = money + (money*interest)
+
+    print ( "$%s" % addFormatting(float(sys.argv[1])), "growing at",percentage(interest), "for", years, "years yields", addFormatting(money), "dollars!")
+
+except SyntaxError:
+    print ("Failure: %s" % SyntaxError)    
+    exit()
